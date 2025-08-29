c302f27024e1ea9c92c4adcd01f50ab9b6d4e157
Sujith George <sujith@byteblazer.com>
2025-08-30 02:17:04 +0530

    removed build info string

--- app/src/main/java/com/deltasoft/pharmatracker/MainActivity.java
+++ app/src/main/java/com/deltasoft/pharmatracker/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
 
         // ✅ Show BuildConfig value
         TextView tv = findViewById(R.id.helloText);
-        tv.setText("Hello from Java!\nAPI = " + BuildConfig.BASE_API_URL);
+        tv.setText("Hello from Java!\nAPI = ");
     }
 }
