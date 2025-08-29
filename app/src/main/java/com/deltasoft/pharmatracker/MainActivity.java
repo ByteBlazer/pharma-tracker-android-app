bcb47511873fe2f3ae865bd391a18e92190df619
Sujith George <sujith@byteblazer.com>
2025-08-30 01:39:03 +0530

    kotlin to java

---
+++ app/src/main/java/com/deltasoft/pharmatracker/MainActivity.java
@@ -0,0 +1,18 @@
+package com.deltasoft.pharmatracker;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // ✅ Show BuildConfig value
+        TextView tv = findViewById(R.id.helloText);
+        tv.setText("Hello from Java!\nAPI = " + BuildConfig.BASE_API_URL);
+    }
+}
