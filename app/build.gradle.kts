2cb85eb58b40ddc1e2ef794a9b26560928e60a54
Sujith George <sujith@byteblazer.com>
2025-08-30 16:35:35 +0530

    sign config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,10 +19,13 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("debug")//TODO: Change
+
         }
     }
 
