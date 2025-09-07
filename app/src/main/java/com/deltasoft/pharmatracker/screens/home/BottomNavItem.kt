280eab55f272789f778ec7b0739f6cd39362ba69
Sreenath K <sreenath@techgentsia.com>
2025-09-07 23:41:10 +0530

    bottom nav visibility issue fix

--- app/src/main/java/com/deltasoft/pharmatracker/screens/home/BottomNavItem.kt
+++ app/src/main/java/com/deltasoft/pharmatracker/screens/home/BottomNavItem.kt
@@ -16,31 +16,38 @@ sealed class BottomNavItem(
     object Scan : BottomNavItem(
         "scan", Icons.Default.Home, "Scan",
         setOf(
-            UserType.WEB_ACCESS,
-            UserType.APP_SCANNER,
-            UserType.APP_TRIP_CREATOR,
             UserType.APP_ADMIN,
-            UserType.APP_TRIP_DRIVER
+            UserType.APP_TRIP_CREATOR,
+            UserType.APP_SCANNER
         )
     )
 
     object RouteQueue : BottomNavItem(
         "route_queue", Icons.Default.AccountBox, "Route Queue",
-        setOf(UserType.APP_TRIP_CREATOR, UserType.APP_ADMIN)
+        setOf(
+            UserType.APP_ADMIN,
+            UserType.APP_TRIP_CREATOR
+        )
     )
 
     object ScheduledTrips : BottomNavItem(
         "scheduled_trips", Icons.Default.AddCircle, "Scheduled Trips",
-        setOf(UserType.APP_SCANNER, UserType.APP_ADMIN, UserType.APP_TRIP_DRIVER)
+        setOf(
+            UserType.APP_ADMIN,
+            UserType.APP_TRIP_CREATOR
+        )
     )
 
     object Drive : BottomNavItem(
         "drive", Icons.Default.LocationOn, "Drive",
-        setOf(UserType.APP_SCANNER, UserType.APP_ADMIN, UserType.APP_TRIP_DRIVER)
+        setOf(
+            UserType.APP_ADMIN,
+            UserType.APP_TRIP_DRIVER
+        )
     )
 
     object Profile : BottomNavItem(
         "profile", Icons.Default.Person, "Profile",
-        setOf(UserType.APP_SCANNER, UserType.APP_ADMIN, UserType.APP_TRIP_DRIVER)
+        setOf(UserType.APP_ADMIN)
     )
 }
