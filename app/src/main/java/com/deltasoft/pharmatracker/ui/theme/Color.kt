b7a35b3f17faa6d7e9b8efb0ac76629d8efc5c4d
Sreenath K <sreenath@techgentsia.com>
2025-10-08 23:10:55 +0530

    App icon color updated

--- app/src/main/java/com/deltasoft/pharmatracker/ui/theme/Color.kt
+++ app/src/main/java/com/deltasoft/pharmatracker/ui/theme/Color.kt
@@ -2,10 +2,20 @@ package com.deltasoft.pharmatracker.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+//val Purple80 = Color(0xFFD0BCFF)
+//val PurpleGrey80 = Color(0xFFCCC2DC)
+//val Pink80 = Color(0xFFEFB8C8)
+//
+//val Purple40 = Color(0xFF6650a4)
+//val PurpleGrey40 = Color(0xFF625b71)
+//val Pink40 = Color(0xFF7D5260)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+
+// Your Primary Color: #084C50 (Dark Teal)
+val AppPrimary = Color(0xFF084C50)
+
+// Your Secondary Color: #49979b (Medium Teal/Cyan)
+val AppSecondary = Color(0xFF49979b)
+
+// Your Tertiary Color: #6FBABD (Light Blue/Cyan)
+val AppTertiary = Color(0xFF6FBABD)
